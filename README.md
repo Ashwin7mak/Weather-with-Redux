2932b54dc38d5b0e9af01aaf233f5bd0a8985eed
Your Name <kelvin.lightner@gmail.com>
2017-02-14 11:13:37 -0800

    Bind context of Search Bar Input

--- README.md
+++ README.md
@@ -21,5 +21,8 @@ Checkout this repo, install dependencies, then start the gulp process with the f
 ```
 
 ####Concepts Notes#####
-Controlled Component(Button) - a form element where the value of the input is set by the state of our component.
+Controlled Component(Button) - a form element where the value of the input is set by the state of our component. Please reference the search bar component.
+
+This and Callback Functions - whenever we hand a callback function like 'this' off and then the callback reference is 'this'. 
+'This' is going to have the incorrect context. The dilemma can be solved by binding it to the constext in the constructor function. Please reference the search bar component.
 
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-	// the constructor function is making the button a controlled field
 	constructor(props) {
 		super(props);
 
 		this.state = { term: ''};
+
+		this.onInputChange = this.onInputChange.bind(this);
+		// breakdown of the above line of code:
+			// this (which is our instance of search bar) has a function called onInputChange.
+			// bind that function to 'this' (which is search bar) and then replace onInputChange with this
+			// new bound instance of this function
 	}
 
 	// all dom handlers come with an event object 
+	// the below function is making the input a controlled field
 	onInputChange(event) {
-		console.log(event.target.value);
+		// console.log(event.target.value);
+		this.setState({ term: event.target.value });
+	}
+
+	onFormSubmit() {
+		
 	}
 
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
 					placeholder="Get a 10-Day Forecast by Searching for Your City"
 					className="form-control"
