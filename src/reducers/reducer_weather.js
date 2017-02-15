b6ac16bcb44de66e0c2574ecc11e7564e7499044
Your Name <kelvin.lightner@gmail.com>
2017-02-14 16:31:25 -0800

    Storing returned data into an array to be used by a list

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,12 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action) {
 	// console.log('action received', action);
-	
+	switch (action.type) {
+		case FETCH_WEATHER:
+			// inserting data into state array
+			return [ action.payload.data, ...state ];
+	}
+
 	return state;
 }
